450d053d34d7f28845a2b37d7000b5ec2490ac4d
Riley Wilton <riley.j.wilton@gmail.com>
2023-05-11 16:39:16 -0400

    Now just need to implement for core types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
+use info::{ScalarType, TypeInfo};
+
 
 /// This module provides type info structures and enums
 pub mod info;
 
 
 /// The main trait of this library which allows introspection on both structs and enums
-pub trait Introspect {
+pub trait Introspectable {
     /// Returns type info for the implemented type.
     fn introspect() -> info::TypeInfo;
+}
+
+
+impl Introspectable for u8 {
+    fn introspect() -> TypeInfo {
+        TypeInfo::Scalar(ScalarType::U8)
+    }
+}
+
+impl Introspectable for u16 {
+    fn introspect() -> TypeInfo {
+        TypeInfo::Scalar(ScalarType::U16)
+    }
+}
+
+impl Introspectable for u32 {
+    fn introspect() -> TypeInfo {
+        TypeInfo::Scalar(ScalarType::U32)
+    }
 }
